54330ce40dd8d11df6ab22baf5dc9c1970b1538e
Christina Leuci <christinaleuci@gmail.com>
2014-10-29 20:37:21 -0400

    worked on css and pictures

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,11 +8,10 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            var name = $("input#name").val(),
+                email = $("input#email").val(),
+                message = $("textarea#message").val(),
+                firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
